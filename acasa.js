eb800ca1eae2fc0820f14026891987dc7811e8c6
brienlandia <86531577+brienlandia@users.noreply.github.com>
2021-06-26 21:09:37 +0300

    Add files via upload

---
+++ acasa.js
@@ -0,0 +1,12 @@
+function redirectionare(numeMasina){
+    window.location.href="catalog/catalog.html?numeMasina="+numeMasina;
+ 
+ 
+ 
+ 
+ }
+   /* Pana aici am scris pe 13.03.2020 */
+
+//    CatHands.start(document.body, {
+//     title: 'Click & Hold'
+// });
